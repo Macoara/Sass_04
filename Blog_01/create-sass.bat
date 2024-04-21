9405ec9db97287c8a8f02672f7eb724b64963f9f
Marisol Cortés <marisol.cortes.apr@gmail.com>
2024-04-21 19:37:14 -0400

    Segundo intento de uso metodología Sass

--- Blog_01/create-sass.bat
+++ Blog_01/create-sass.bat
@@ -5,33 +5,42 @@ mkdir sass
 cd sass
 mkdir base components layout pages themes abstracts vendors
 cd base
-echo /* Base styles here */ > _base.scss
-cd..
+
+echo @import 'abstracts/mixins'; > main.scss
+echo @import 'abstracts/variables'; >> main.scss
+echo @import 'base/base'; >> main.scss
+echo @import 'base/typography'; >> main.scss
+echo @import 'components/buttons'; >> main.scss
+echo @import 'components/input-proups'; >> main.scss
+echo @import 'components/links'; >> main.scss
+echo @import 'layaut/header'; >> main.scss
+echo @import 'layaut/main'; >> main.scss
+cd
 
 cd components
 echo /* Components styles here */ > _buttons.scss
-cd..
+cd
 
 cd layout
 echo /* Layout styles here */ > _header.scss
-cd..
+cd
 
 cd pages
 echo /* Page specific styles here */ > _home.scss
-cd..
+cd
 
 cd themes
 echo /* Themes styles here */ > _theme.scss
-cd..
+cd
 
 cd abstracts
 echo /* Functions, mixins, variables here */ > _mixins.scss
-cd..
+cd
 
 cd vendors
 echo /* Third party styles here */ > _bootstrap.scss
-cd..
+cd
 
-cd..
+cd
 echo Estructura creada exitosamente.
 pause
